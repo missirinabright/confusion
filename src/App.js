abe11a032426929579ca0d78cd8afc32367d2f7c
github username <github email address>
2019-08-31 00:13:06 -0400

    Adds state and onClick

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-// import './App.css';
+import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-      </Navbar>
-      <div className="container">
-        <NavbarBrand href="/">
-          Irina Ristorante Con Fusion
-        </NavbarBrand>
-        <Menu />
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+        </Navbar>
+        <div className="container">
+          <NavbarBrand href="/">
+            Irina Ristorante Con Fusion
+          </NavbarBrand>
+          <Menu dishes={this.state.dishes}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
